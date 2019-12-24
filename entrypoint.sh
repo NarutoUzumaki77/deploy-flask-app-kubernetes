a2322927dcfca9f488ecf27f7c6c2a5944d0ea46
Gilbert Nwankwo <kellyemeka7@gmail.com>
2019-12-24 10:42:38 -0500

    ran the image locally, using gunicorn server

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 
-mkdir -p /deploy/logs
-touch /deploy/logs/error.log
-touch /deploy/logs/access.log
-cd /deploy
-exec gunicorn --bind 0.0.0.0:8080 -c docker-gunicorn.conf.py main:app
+exec gunicorn -b 0.0.0.0:8080 --access-logfile - --error-logfile - main:APP
